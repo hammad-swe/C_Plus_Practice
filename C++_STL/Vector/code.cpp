66ba02251a2c24b1965051b6a215252e607d3637
Hammad Ali <hammadali78056@gmail.com>
2026-01-27 14:51:56 +0500

    feat: C++ STL practices

---
+++ C++_STL/Vector/code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> vec;
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+
+    for(int val : vec){
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
