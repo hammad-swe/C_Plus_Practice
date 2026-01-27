ccfd60b6654fb6e9e5614376c957296673310a65
Hammad Ali <hammadali78056@gmail.com>
2026-01-27 15:48:46 +0500

    Pract: C++ STL Vectors

--- C++_STL/Vector/code.cpp
+++ C++_STL/Vector/code.cpp
@@ -12,6 +12,30 @@ int main(){
         cout << val << " ";
     }
     cout << endl;
+// at() or []
+cout << "Element at index 1: " << vec.at(1)<< " " << vec[1] << endl;
+// front and back
+cout<<"First Element:"<< vec.front() << endl;
+cout<< "Last Element:" << vec.back() << endl;
 
-    return 0;
+//size and capacity
+cout<<"Size: "<< vec.size() << endl;
+cout<<"Capacity: "<< vec.capacity() << endl;
+
+// new way of presentation
+vector<int> vec2(3,10); // 3 elements with value 10
+for(int val : vec2){
+    cout << val << " "<< endl;
+}
+
+// erase , clear, insert, empty
+vec2.insert(vec2.begin()+1, 15); // insert 15 at index 1
+cout<<"After Insertion: ";
+
+for(int val : vec2){
+    cout << val << " ";
+}
+cout << endl;
+vec2.erase(vec2.begin()); // erase element at index 3
+return 0;
 }
